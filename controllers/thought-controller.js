b545ab069c0407b6a9ad0fac73605333eb93f6d4
Shannon <shogeboom187@gmail.com>
2023-07-19 20:03:22 -0700

    added comments to controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,6 +4,7 @@ module.exports = {
     // gets all thoughts
     async getThoughts(req,res){
         try{
+            // thoughts is assigned to the result of the find method
             const thoughts = await Thought.find();
             res.json(thoughts);
         }catch(err){
@@ -13,6 +14,7 @@ module.exports = {
     // gets specified thought 
     async getSingleThought(req,res){
         try{
+            // thought is assigned to the result of the findOne method
             const thought = await Thought.findOne({_id: req.params.thoughtId}).select('-__v');
             if (!thought){
                 return res.status(404).json({message: 'No thought with that ID'});
@@ -25,6 +27,7 @@ module.exports = {
     // creates a thought
     async createThought(req,res){
         try{
+            // thought is assigned to the result of the create method
             const thought = await Thought.create(req.body);
             res.json(thought);
         }
@@ -36,11 +39,13 @@ module.exports = {
     // deletes a thought
     async deleteThought(req,res){
         try{
+            // thought is assigned to the result of the findOneAndDelete method
             const thought = await Thought.findOneAndDelete({_id: req.params.thoughtId});
 
             if(!thought){
                 res.status(404).json({message: 'No thought with that ID'});
             }
+            // deletes the thought from the user's thoughts array
             await User.deleteMany({_id:{$in: thought.user}});
             res.json({message:''})
         }
@@ -53,14 +58,18 @@ module.exports = {
     //updates thought
     async updateThought(req, res) {
         try{
+            // thought is assigned to the result of the findOneAndUpdate method
             const thought = await Thought.findOneAndUpdate(
+                // finds the thought by id
                 {_id: req.params.thoughtId},
+                // updates the thought
                 {$set: req.body},
                 {runValidators: true, new: true}
             );
             if(!thought){
                 return res.status(404).json({message: 'No thought with this id'});
             }
+            // returns the updated thought
             res.json(thought);
         }
         catch(err){
@@ -71,8 +80,11 @@ module.exports = {
     // post a reaction
     async createReaction(req,res){
         try{
+            // thought is assigned to the result of the findOneAndUpdate method
            const thought = await Thought.findOneAndUpdate(
+            // finds the thought by id
                 {_id: req.params.thoughtId},
+                // pushes the reaction to the reactions array
                 {$push: {reactions: req.body}},
                 {new: true},
             )
@@ -88,8 +100,11 @@ module.exports = {
 
     async deleteReaction(req,res){
         try{
+            // thought is assigned to the result of the findOneAndUpdate method
             const thought = await Thought.findOneAndUpdate(
+                // finds the thought by id
                 {_id: req.params.thoughtId},
+                // pulls the reaction from the reactions array
                 {$pull:{reactions:{reactionId: req.params.reactionId}}},
                 {new: true},
             )
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,6 +6,7 @@ module.exports = {
     //find all users
     async getAllUsers(req, res) {
         try {
+            // users is assigned to the result of the find method
             const users = await User.find({}).select('-__v');
             res.json(users);
         } catch (err) {
@@ -17,6 +18,7 @@ module.exports = {
     //get single user 
     async getSingleUser(req, res) {
         try {
+            // user is assigned to the result of the findOne method
             const user = await User.findOne({ _id: req.params.userId }).select('-__v');
             if (!user) {
                 return res.status(404).json({ message: 'no user with this id' });
@@ -31,6 +33,7 @@ module.exports = {
     //creates a user
     async createUser(req, res) {
         try {
+            // user is assigned to the result of the create method
             const user = await User.create(req.body);
             res.json(user);
         }
@@ -41,12 +44,16 @@ module.exports = {
     //delete and remove thoughts associated with user
     async deleteUser(req, res) {
         try {
+            // user is assigned to the result of the findOneAndDelete method
             const user = await User.findOneAndRemove({ _id: req.params.userId });
             if (!user) {
                 res.status(404).json({ message: 'user DNE' });
             }
+            // deletes the user from the thoughts array
             const thought = await Thought.findOneAndUpdate(
+                // finds the thought with the user id
                 { user: req.params.userId },
+                // removes the user from the thoughts array
                 { $pull: { user: req.params.userId } },
                 { new: true }
             );
@@ -64,7 +71,9 @@ module.exports = {
     // Update user
     async updateUser(req, res) {
         try {
+            // user is assigned to the result of the findOneAndUpdate method
             const user = await User.findOneAndUpdate(
+                // finds the user with the id
                 { _id: req.params.id },
                 { new: true },
                 { new:true, runValidators: true }
@@ -81,8 +90,11 @@ module.exports = {
     //add friend
     async addFriend(req, res) {
         try {
+            // user is assigned to the result of the findOneAndUpdate method
             const user = await User.findByIdAndUpdate(
+                // finds the user with the id
                req.params.userId,
+            //    adds the friend to the friends array
                 { $addToSet: { friends: req.params.friendsId } },
                 { new: true, runValidators: true},
             )
@@ -99,8 +111,11 @@ module.exports = {
     //remove friend
     async removeFriend(req, res) {
         try {
+            // user is assigned to the result of the findOneAndUpdate method
             const user = await User.findByIdAndUpdate(
+                // finds the user with the id
                 req.params.userId,
+                // removes the friend from the friends array
                 { $pull: { friends: req.params.friendsId } },
                 { new: true, runValidators: true }
             ).select("-__v");
